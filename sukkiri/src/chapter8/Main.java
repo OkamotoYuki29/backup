287c32ed803c214ae4a33bb8f44d27f985bb0190
OkamotoYuki29 <okamoto.macho@gmail.com>
2017-12-19 16:24:53 +0900

    12/19

---
+++ sukkiri/src/chapter8/Main.java
@@ -0,0 +1,12 @@
+package chapter8;
+
+public class Main {
+	public static void main(String[] args){
+		Cleric c = new Cleric();
+		c.name = "ÉAÉJÉl";
+		c.hp = 20;
+		c.mp = 5;
+		c.selfAid();
+		c.pray(3);
+	}
+}
