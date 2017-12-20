7115ba5e0a4179d7b2be894fb3b0e93dc8920d15
OkamotoYuki29 <okamoto.macho@gmail.com>
2017-12-20 15:04:56 +0900

    12/20

---
+++ sukkiri/src/chapter12/TangibleAsset.java
@@ -0,0 +1,16 @@
+package chapter12;
+
+public abstract class TangibleAsset extends Asset implements Thing{
+	private String color;
+	private double weight;
+	//コンストラクタ
+	public TangibleAsset
+		(String name, int price, String color){
+		super(name,price);
+		this.color = color;
+	}
+	//gettersetterメソッド
+	public String getColor(){return this.color;}
+	public double getWeight(){return this.weight;}
+	public void setWeight(double weight){this.weight = weight;}
+}
