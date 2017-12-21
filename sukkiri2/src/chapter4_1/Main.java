1393cbe8b39cda4fbf345980720027fa773f3ffc
OkamotoYuki29 <okamoto.macho@gmail.com>
2017-12-21 16:50:44 +0900

    12/21

---
+++ sukkiri2/src/chapter4_1/Main.java
@@ -0,0 +1,10 @@
+package chapter4_1;
+
+public class Main {
+	public static void main(String[] args){
+		int [] points = new int[4];
+		double [] weiths = new double[5];
+		boolean[] answers = new boolean[3];
+		String[] names = new String[3];
+	}
+}
