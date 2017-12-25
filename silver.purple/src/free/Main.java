0920488b53b0ab607781a3e75f17fe76b9b69042
OkamotoYuki29 <okamoto.macho@gmail.com>
2017-12-25 17:16:40 +0900

    12/25

--- silver.purple/src/free/Main.java
+++ silver.purple/src/free/Main.java
@@ -2,13 +2,11 @@ package free;
 
 public class Main{
 	public static void main(String[] args){
-		StringBuilder sb1 = new StringBuilder("Ab Cdefge");
-		System.out.println(sb1.append("XYZ"));
-		System.out.println(sb1.insert(2,"ZZ"));
-		System.out.println(sb1.reverse());
-		StringBuilder sb2 = new StringBuilder("Ab Cdefge");
-		sb2.setCharAt(2, 'Z');
-		System.out.println(sb2);
-		System.out.println(sb1.length());
+		int i1 = 100;
+		Integer obj = i1;
+		obj = 200;
+		System.out.println(obj);
+		System.out.println(i1);
 	}
 }
+
