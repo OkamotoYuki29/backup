dfc6f684c246810f83f9daff065cfc0a4446d466
OkamotoYuki29 <okamoto.macho@gmail.com>
2018-01-09 17:51:45 +0900

    0109_1

--- silver.purple/src/free/Main.java
+++ silver.purple/src/free/Main.java
@@ -1,16 +1,8 @@
 package free;
 
 public class Main{
-	public static void main(String[] args) {
-		StringBuilder s = new StringBuilder();
-		for(int i = 1; i <= 100; i++) {
-			s.append(i).append(",");
-		}
-		System.out.println(s);
-		String s1 = s.toString();
-		String[] a = s1.split("[,]");
-		for(String c : a) {
-			System.out.println(c);
-		}
+	public static void main(String[] args){
+		
 	}
 }
+
